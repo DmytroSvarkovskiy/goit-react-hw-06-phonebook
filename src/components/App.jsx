df01c516c1b5b547c27fc107803f81bd7e143f42
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-12-26 22:16:18 +0200

    did task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,9 @@
-import { createGlobalStyle } from 'styled-components';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Contactlist } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { useSelector } from 'react-redux';
+import { GlobalStyle } from './GlobalStyle';
 
-const GlobalStyle = createGlobalStyle`
-  ul,h1,h2,h3,h4,h5,h6,li,p{list-style:none;margin:0;padding:0;};
-  body{
-   margin-top:50px;
-   display: flex;
-   justify-content:center;
-   align-items:center;
-   color: '#010101'; 
-  }
-`;
 export const App = () => {
   const contacts = useSelector(state => state.contacts);
 
