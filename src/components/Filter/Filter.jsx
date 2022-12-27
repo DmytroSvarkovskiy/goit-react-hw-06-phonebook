bdc553e30a48e0f4c6e65e446de79d85197fe024
Дмитро Сварковський <100209541+DmytroSvarkovskiy@users.noreply.github.com>
2022-12-27 10:25:43 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,13 +1,15 @@
 import { WrapperFiler, FilterInput, FilterP } from './Filter.styled';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { filtration } from 'Redux/filterSlice';
 export const Filter = () => {
   const dispatch = useDispatch();
+  const filter = useSelector(state => state.filter);
 
   return (
     <WrapperFiler>
       <FilterP>Find contacts by name</FilterP>
       <FilterInput
+        value={filter}
         type="text"
         onChange={e => dispatch(filtration(e.target.value))}
       />
